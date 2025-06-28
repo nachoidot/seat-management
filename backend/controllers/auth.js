aefe56c22914148fca12c24922cb5cb7d41da583
jaewonE <importjaewonE@gmail.com>
2025-06-28 13:53:38 +0900

    set sameSite

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -87,7 +87,7 @@ exports.login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30일
     });
 
